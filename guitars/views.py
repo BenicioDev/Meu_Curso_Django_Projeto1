fc168b27591b67f6a848bbf9ac6219f26f4a1947
Gabriel Benicio <gabrielvitor083@gmail.com>
2022-11-30 00:08:49 -0300

    views.py

--- guitars/views.py
+++ guitars/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+# Função Render renderiza a Pagina HTML na pasta Templates
+
 
 def home(request):
+    # Return solicita o render que recebe um request com o nome do Arquivo
+    # HTML django, no caso 'home.html'
     return render(request, 'home.html')
 
 
